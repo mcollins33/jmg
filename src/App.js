06b6ac14395178d64570a92838ebb1a74d6db51d
mcollins33 <collins.miki@gmail.com>
2019-02-09 12:50:15 -0500

    add hero

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Hero from './components/Hero/Hero';
 import Navbar from './components/Navbar/Navbar';
 import Card from './components/Card/Card';
 
@@ -8,6 +9,7 @@ class App extends Component {
     return (
       <div className="App">
       <Navbar />
+      <Hero />
       <Card />
         
       </div>
---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './Hero.css';
+
+const Hero = () => {
+
+    return (
+<div className="hero is-fullheight">
+	<div className="hero-body">
+		<div className="container has-text-centered">
+			<p className="title">
+				Say Something About Company
+			</p>
+			<p className="subtitle">
+				Say Something Else
+			</p>
+		</div>
+	</div>
+</div>
+
+
+    );
+};
+
+export default Hero;
